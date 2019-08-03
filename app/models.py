8de54a795532234cee0ba6fd656ce2476875aa3f
Alexius Academia <alexius.academia@gmail.com>
2019-08-04 00:06:38 +0800

    Changed length limit of password

--- app/models.py
+++ app/models.py
@@ -42,7 +42,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     user_type_id = db.Column(db.Integer, db.ForeignKey('user_type.id'), nullable=False, default=1)
     user_type = db.relationship('UserType', backref='user_type', lazy=True)
     last_login = db.Column(db.DateTime)
