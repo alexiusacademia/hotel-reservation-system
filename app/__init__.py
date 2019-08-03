2d2518d8f34a438f5010913fa19552dbe355b8d1
Alexius Academia <alexius.academia@gmail.com>
2019-08-03 19:52:22 +0800

    Added configs for the app object and initialized marshmallow instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 import os
 
 
-base_dir = os.path.abspath(os.path.dirname(__file__))
+# base_dir = os.path.abspath(os.path.dirname(__file__))
+base_dir = os.path.abspath(os.getcwd())
 
-
+# Initialize app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + base_dir + 'db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Initialize db
 db = SQLAlchemy(app)
+
+# Initialize ma
+ma = Marshmallow(app)
