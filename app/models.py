df3dcee87534f457c6eb28ff8494c95a3f7853ae
Alexius Academia <alexius.academia@gmail.com>
2019-08-03 19:45:56 +0800

    Created table Room and its schema

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from app import db, ma
+
+
+# Room Table
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    qty = db.Column(db.Integer)
+    price = db.Column(db.Float, nullable=False)
+    available_rooms = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"Room(Name:{self.name}, Qty:{self.qty}, Price:{self.price}, Available:{self.available_rooms})"
+
+
+# Room Schema
+class RoomSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'qty', 'price', 'available_rooms')
+
+
+# Initialize schema
+room_schema = RoomSchema(strict=True)
+rooms_schema = RoomSchema(many=True, strict=True)
