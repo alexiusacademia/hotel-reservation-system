58720d49711c40a0bec8d7da4fe85a147d1e3f0e
Alexius Academia <alexius.academia@gmail.com>
2019-08-04 10:39:38 +0800

    Used sendmail in sending email.

--- app/routes/send_mail.py
+++ app/routes/send_mail.py
@@ -1,11 +1,23 @@
 from app import app, mail, Message
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
 
 
 @app.route('/send_mail')
 def send_mail():
+    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email('syncsoftsolutions.software@gmail.com')
+    subject = 'Test Subject using Sendgrid'
+    to_email = Email('alexius.academia@gmail.com')
+    content = Content('text/plain', 'Hello! This is a test mail using sendgrid.')
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    '''
     msg = Message(subject='Test Subject',
                   recipients=['alexius.academia@gmail.com'],
                   body='This is a test message 2',
                   sender='syncsoftsolutions.software@gmail.com')
     mail.send(msg)
+    '''
     return 'Email sent!'
