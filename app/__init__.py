ae141712ee2de6de9cc90844fa1d317736354721
Alexius Academia <alexius.academia@gmail.com>
2019-08-04 09:29:24 +0800

    Used environment variables for email configs

--- app/__init__.py
+++ app/__init__.py
@@ -17,8 +17,9 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'syncsoftsolutions.software'
-app.config['MAIL_PASSWORD'] = 'M@y31l9BB'
+app.config.from_envvar('EMAIL_USERNAME')
+app.config.from_envvar('EMAIL_PASSWORD')
+app.config['SECRET_KEY'] = ''
 
 # Initialize db
 db = SQLAlchemy(app)
