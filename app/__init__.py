b009c7d023f5cfba7b7aac41e62a7133cc29a248
Alexius Academia <alexius.academia@gmail.com>
2019-08-04 14:16:27 +0800

    Added imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
