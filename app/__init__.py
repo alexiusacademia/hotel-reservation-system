11409529bf3cc68b3837dc6b76f46433943bf959
Alexius Academia <alexius.academia@gmail.com>
2019-08-04 09:06:27 +0800

    Added configs and init for email sending

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
+from flask_mail import Mail, Message
 import os
 
 
@@ -12,6 +13,12 @@ base_dir = os.path.abspath(os.getcwd())
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'syncsoftsolutions.software'
+app.config['MAIL_PASSWORD'] = 'M@y31l9BB'
 
 # Initialize db
 db = SQLAlchemy(app)
@@ -21,3 +28,6 @@ ma = Marshmallow(app)
 
 # Initialize bcrypt
 bcrypt = Bcrypt(app)
+
+# Initialize Mail
+mail = Mail(app)
