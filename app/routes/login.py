0dac21a93f9b0db281f3f42cc51975ebe7eccd15
Alexius Academia <alexius.academia@gmail.com>
2019-08-04 20:00:09 +0800

    Changed request to authorization

--- app/routes/login.py
+++ app/routes/login.py
@@ -1,14 +1,14 @@
-from app import app, bcrypt, request, jsonify, make_response
+from app import app, bcrypt, request, jsonify, make_response, jwt
 from app.models import User, user_schema
-import jwt
 import datetime
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    auth = request.json
-    email = auth['email']
-    password = auth['password']
+    # Get the request
+    auth = request.authorization
+    email = auth.username
+    password = auth.password
 
     # Check if user exist
     user = User.query.filter_by(email=email).first()
@@ -23,7 +23,7 @@ def login():
             token = jwt.encode({'user_email': email,
                                 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
                                str(app.config['SECRET_KEY']))
-            print(app.config['SECRET_KEY'])
+
             # Decode to regular string
             token = token.decode('utf-8')
 
