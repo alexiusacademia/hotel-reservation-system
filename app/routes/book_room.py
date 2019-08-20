9333a68e6fcdbd26c2dd25722170ba2587938d28
Alexius Academia <alexius.academia@gmail.com>
2019-08-20 20:52:41 +0800

    Added error checking for argument room_id.

--- app/routes/book_room.py
+++ app/routes/book_room.py
@@ -3,8 +3,11 @@ from app import app, request, jsonify
 
 @app.route('/book')
 def book():
+    if 'room_id' not in request.args:
+        return jsonify({'success': False, 'message': 'room_id must be in the args.'})
+
     room_id = request.args['room_id']
 
     print(room_id)
 
-    return jsonify(request.args)
+    return jsonify({'success': True})
