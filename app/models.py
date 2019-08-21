0a2c027f0c94b9e83b6329445efbd7ee078e1ff7
Alexius Academia <alexius.academia@gmail.com>
2019-08-21 17:51:01 +0800

    Added repr

--- app/models.py
+++ app/models.py
@@ -30,10 +30,13 @@ class RoomType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     price = db.Column(db.Float, nullable=False)
-    
+
     def __init__(self, name, price):
         self.name = name
         self.price = price
+
+    def __repr__(self):
+        return f"RoomType(Name:{self.name}, Price:{self.price})"
 
 
 class UserType(db.Model):
