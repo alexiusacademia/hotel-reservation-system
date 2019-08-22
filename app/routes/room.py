b9f6243cf791253c452ef571c01bcbce81966186
Alexius Academia <alexius.academia@gmail.com>
2019-08-22 16:59:41 +0800

    Added checking for existing room

--- app/routes/room.py
+++ app/routes/room.py
@@ -7,7 +7,14 @@ from app.models import Room, room_schema, RoomType
 def room(id):
     room = Room.query.get(id)
 
+    if not room:
+        return jsonify({
+            'success': False,
+            'message': f'Room with the id {id} dows not exist.'
+        })
+
     _room = {}
+    _room['success'] = True
     _room['id'] = room.id
     _room['available'] = room.available
     _room['room_number'] = room.room_number
