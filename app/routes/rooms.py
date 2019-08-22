7b868e836471a8f23b1d5c029a603b7070c2dab3
Alexius Academia <alexius.academia@gmail.com>
2019-08-22 19:57:38 +0800

    Show rooms from a given type

--- app/routes/rooms.py
+++ app/routes/rooms.py
@@ -20,3 +20,41 @@ def rooms():
         _rooms_list.append(_room)
         
     return jsonify(_rooms_list)
+
+
+@app.route('/rooms/<int:type_id>')
+def rooms_category(type_id):
+    try:
+        room_type_id = int(type_id)
+    except Exception as e:
+        return jsonify({
+            'success': False,
+            'message': 'Room type id must be an integer.'
+        })
+
+    room_type = RoomType.query.get(room_type_id)
+
+    if not room_type:
+        return jsonify({
+            'success': False,
+            'message': f'Room type with id {room_type_id} does not exist.'
+        })
+
+    rooms = Room.query.filter_by(type_id=room_type_id).all()
+
+    _rooms = []
+    for room in rooms:
+        _room = {}
+        _room['id'] = room.id
+        _room['available'] = room.available
+        _room['room_number'] = room.room_number
+        _room['price'] = room.type.price
+        _room['type'] = room.type.name
+        _rooms.append(_room)
+
+    obj = {}
+
+    obj['success'] = True
+    obj['rooms'] = _rooms
+
+    return jsonify(obj)
