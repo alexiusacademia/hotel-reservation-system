ffe8ce0b8794789172373cc13aa59572097d9d10
Alexius Academia <alexius.academia@gmail.com>
2019-08-22 16:51:44 +0800

    Updated the json return of room

--- app/routes/room.py
+++ app/routes/room.py
@@ -1,10 +1,17 @@
-from app import app, token_required
-from app.models import Room, room_schema
+from app import app, token_required, jsonify
+from app.models import Room, room_schema, RoomType
 
 
 @app.route('/room/<int:id>')
 # @token_required
 def room(id):
-    _room = Room.query.get(id)
+    room = Room.query.get(id)
 
-    return room_schema.jsonify(_room)
+    _room = {}
+    _room['id'] = room.id
+    _room['available'] = room.available
+    _room['room_number'] = room.room_number
+    _room['price'] = room.type.price
+    _room['type'] = room.type.name
+
+    return jsonify(_room)
